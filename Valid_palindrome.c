9e25cd8e378c6485487b2bffa6b6d232c239311f
Aishwarya R <aishwaryar.bs24@bmsce.ac.in>
2026-03-02 19:18:36 +0530

    Create Valid_palindrome.c

---
+++ Valid_palindrome.c
@@ -0,0 +1,28 @@
+#include<ctype.h>
+#include<string.h>
+bool isPalindrome(char* s) {
+    int left=0;
+    int right= strlen(s)-1;
+        while(left<right)
+        {
+         if(!isalnum(s[left]))
+          {
+            left++;
+          }
+          else if(!isalnum(s[right]))
+          {
+            right--;
+          }  
+          else if(tolower(s[left])!=tolower(s[right]))
+          {
+            return false;
+          }
+          if(tolower(s[left])==tolower(s[right]))
+          {
+             left++;
+            right--;
+          }
+          
+        }
+    return true;    
+}
